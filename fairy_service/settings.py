d547591ab4394fc180ef96c89cd9a2398d035327
zhao jingyu <dlzhaojy@cn.ibm.com>
2018-01-14 22:07:03 +0800

    Project update~

--- fairy_service/settings.py
+++ fairy_service/settings.py
@@ -121,7 +121,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'templates/build/static'),
 )
+MIDDLEWARE_CLASSES = (
+    # Simplified static file serving.
+    # https://warehouse.python.org/project/whitenoise/
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
