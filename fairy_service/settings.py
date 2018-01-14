f648eeb234402d3029ef11ce8e5f564fbda19eca
zhao jingyu <dlzhaojy@cn.ibm.com>
2018-01-14 19:35:06 +0800

    Project update~

--- fairy_service/settings.py
+++ fairy_service/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '98g0g$q18=va8&)5+k*s9sw*585rzax=nl_8cj_!6#@zy^m8-%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'templates/build/static'),
 )
